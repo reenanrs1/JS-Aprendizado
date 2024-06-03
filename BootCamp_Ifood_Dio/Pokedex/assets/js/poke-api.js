6522ee3cdc53c6fe333c8b6cdb315d09bdd24f4e
Renan <renan.victor325@outlook.com>
2024-06-03 20:03:20 -0300

    criação do model em js para não precisar puxar do site

--- BootCamp_Ifood_Dio/Pokedex/assets/js/poke-api.js
+++ BootCamp_Ifood_Dio/Pokedex/assets/js/poke-api.js
@@ -1,8 +1,26 @@
 
 const pokeApi = {}
 
+function convertPokeApiDatailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonsDetail = (pokemon) =>{
-    return fetch(pokemon.url).then((response) => response.json())
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDatailToPokemon) 
 }
 
 pokeApi.getPokemons =  (offset = 0, limit = 10 ) =>{
