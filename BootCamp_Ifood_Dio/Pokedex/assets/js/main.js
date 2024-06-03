af320c38e0f7d3b1b41ff98fcaf3f4dc648940bf
Renan <renan.victor325@outlook.com>
2024-06-03 11:16:57 -0300

    atualizações dentro do main e criado mais um arquivo js para parametrização das request

--- BootCamp_Ifood_Dio/Pokedex/assets/js/main.js
+++ BootCamp_Ifood_Dio/Pokedex/assets/js/main.js
@@ -1,1 +1,35 @@
-console.log("Sucesso!!")
+
+
+function convertPokemonToLi(pokemon){
+    return `    <li class = "pokemon">
+                    <span class="number">#001</span>
+                    <span class= "name">${pokemon.name}</span>
+
+                    <div class="detail">
+                        <ol class="types">
+                            <li class="type">planta</li>
+                            <li class="type">veneno</li>
+                        </ol>
+
+                        <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                            alt="${pokemon.name}"> 
+                    </div>
+                        
+                </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
+
+    pokemons.map()
+
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItems.push(convertPokemonToLi(pokemon))
+    }
+
+    console.log(listItems)
+})
+
