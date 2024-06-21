2314f891751c8d5456a02ffbbd4b15f4e484361b
Renan <renan.victor325@outlook.com>
2024-06-21 13:22:41 -0300

    tipos de dados?: boolean, null, undefined

---
+++ BootCamp_Ifood_Dio/01 - Variaveis e operadores/aula2.js
@@ -0,0 +1,41 @@
+/*
+var var1 = 10;
+var var2 = 'Teste';
+console.log(var1 + var2)
+
+
+function o sistema entendente quando vem antes, var não, fazendo o sistema dar erro
+*/
+
+
+/*teste1();
+teste2();
+
+ var teste1 = function(){
+     console.log('Teste 1');
+ }
+
+ function teste2 (){
+     console.log('teste2')
+ }
+
+
+ DIFERENÇAS DO VAR/LET/CONST
+ VAR = Permite atribuição, vaza do escopo, só se restringe a escopo de funções
+ LET = Restringe a qualquer tipo de escopo, ele permite atribuição
+ Const = Restringe a escopo, e não permite atribuição
+
+ Conveção de nomeclatura:
+
+ Pode começar com Letras,
+ Pode começar com dolar $
+ Pode começar com _ : geralmente utilizada quando é restrita aquele escopo
+ Classe ou construtura pode-se começar com letras maiuculas
+ Geralmente sempre são iniciadas com letras minusculas e a segunda palavra iniciada com letra mauisculas
+ Quando é uma constante geralmente indicado com tudo letra maiuscula 
+ 
+ Não pode começar com numeros
+ Não pode ter espaços
+ Não pode ter caracteres especiais
+ 
+*/
---
+++ BootCamp_Ifood_Dio/08_tiposDados/tipos-dados.js
@@ -0,0 +1,24 @@
+/*
+boolean
+null - ausencia de valor
+undefined - não existe - ausencia de declarção
+number
+string
+synbol
+
+
+
+
+//object
+true, false
+
+new Boolean(true) //objeto - classe
+*/
+ 
+const nome = ' ';
+
+if (nome){
+    console.log('Preenchido');
+}else{
+    console.log('Não preenchido');
+}
