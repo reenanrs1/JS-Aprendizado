0be203b3c7790309728cec48e1d3df3ce9d8236b
Renan <renan.victor325@outlook.com>
2024-06-22 17:34:08 -0300

    -Adicionado o flip no card

--- BootCamp_Ifood_Dio/Pokedex/assets/js/main.js
+++ BootCamp_Ifood_Dio/Pokedex/assets/js/main.js
@@ -1,38 +1,45 @@
-       
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 5;
-let offset = 0;
+const pokemonList = document.getElementById('pokemonList');
+        const loadMoreButton = document.getElementById('loadMoreButton');
+        const limit = 5;
+        let offset = 0;
 
+        function loadPokemonsItens(offset, limit) {
+            pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+                const newHtml = pokemons.map((pokemon, index) => `
+                    <div class="flip" id="card-${offset + index}">
+                        <div class="card">
+                            <div class="face front">
+                                <li class="pokemon ${pokemon.type}">
+                                    <span class="number">${pokemon.number}</span>
+                                    <span class="name">${pokemon.name}</span>
+                                    <div class="detail">
+                                        <ol class="types">
+                                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                                        </ol>
+                                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                                    </div>
+                                </li>
+                            </div>
+                            <div class="face back ${pokemon.type}">
+                                FUNDO
+                            </div>
+                        </div>
+                    </div>
+                `).join('');
+                pokemonList.innerHTML += newHtml;
 
+                pokemons.forEach((_, index) => {
+                    const card = document.getElementById(`card-${offset + index}`);
+                    card.addEventListener("click", () => {
+                        card.querySelector('.card').classList.toggle("flip-card");
+                    });
+                });
+            });
+        }
 
-function loadPokemonsItens(offset, limit) {
-    
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map((pokemon) => ` 
-            <li class = "pokemon ${pokemon.type}">
-                         <span class="number">${pokemon.number}</span>
-                         <span class= "name">${pokemon.name}</span>
-     
-                         <div class="detail">
-                             <ol class="types">
-                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                             </ol>
-     
-                             <img src ="${pokemon.photo}"
-                                 alt="${pokemon.name}"> 
-                         </div>
-                             
-                     </li>
-         `
-        ).join('')
-        pokemonList.innerHTML += newHtml
-    })
-}
+        loadPokemonsItens(offset, limit);
 
-loadPokemonsItens(offset, limit)
-
-loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    loadPokemonsItens(offset,limit)
-})
+        loadMoreButton.addEventListener('click', () => {
+            offset += limit;
+            loadPokemonsItens(offset, limit);
+        });
