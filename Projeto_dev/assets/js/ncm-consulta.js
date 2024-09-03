1ce74b9c624eeae0c736904dcdc22d2925b5a4e5
Renan <renan.victor325@outlook.com>
2024-09-03 17:20:42 -0300

    .

--- Projeto_dev/assets/js/ncm-consulta.js
+++ Projeto_dev/assets/js/ncm-consulta.js
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch((error) => {
             ncmResult.innerHTML = `<p>Erro: ${error.message}</p>`;
             if (error.message === 'Failed to fetch') {
-                ncmResult.innerHTML = '<p>Erro: Site de consulta está OF-LINE.</p>';
+                ncmResult.innerHTML = '<p>Erro: Site de consulta está OFFF-LINE.</p>';
             }
         })
         .finally(() => {
